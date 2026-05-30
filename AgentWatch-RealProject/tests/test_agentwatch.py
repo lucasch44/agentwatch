c8fc889e1156f5c93dff2da640990f4b5be43d74
lucasch44 <151957028+lucasch44@users.noreply.github.com>
2026-05-30 15:02:37 -0300

    Delete AgentWatch-RealProject directory

--- AgentWatch-RealProject/tests/test_agentwatch.py
+++
@@ -1,8 +0,0 @@
-
-from agentwatch import LoopDetector
-
-def test_loop_detection():
-    d = LoopDetector()
-    for _ in range(4):
-        assert d.check("run") is False
-    assert d.check("run") is True
